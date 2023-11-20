66b14fa693b6a587b12dd494ecec7ae0ffb8ccd5
kyleslzr <150329661+kyleslzr@users.noreply.github.com>
2023-11-20 22:12:45 +0800

    hahaa

--- index.php
+++ index.php
@@ -141,7 +141,6 @@
           <a href = "https://www.facebook.com/SanJoseAcademyBulacanFalcons" target="_blank"> 
           <img class="logo-image" src="SJAB LOGO.png"  alt="English Adventures Logo">
           </a>
-          <img class="traveler-blink" src="blink.jpg" alt="English Adventures Logo">
           <a href="lesson-list.php">
                <button class="start-button animate__bounceInDown">
                <svg style="margin-right: 15px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#00741e}</style><path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9V344c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/></svg>
